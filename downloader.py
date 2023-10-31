1c75170457caeee8d840c3c7ab6e178c8a2e2a8c
bob <c2064179@newcastle.ac.uk>
2023-10-31 11:29:31 +0000

    change print statement from length to views

--- downloader.py
+++ downloader.py
@@ -5,4 +5,4 @@ link = argv[1]  # Link assigned to first argument from command line
 yt = YouTube(link)  # Create youtube object from the link
 
 print("Title: ", yt.title)
-print("Length: ", yt.length)
+print("Views: ", yt.views)
