90445048d45a3b1bb59c43dfb711fdc7694dd7df
bob <c2064179@newcastle.ac.uk>
2023-10-31 11:45:33 +0000

    created a downloader object yd
    run the downloader object using yd.download

--- downloader.py
+++ downloader.py
@@ -6,3 +6,7 @@ yt = YouTube(link)  # Create youtube object from the link
 
 print("Title: ", yt.title)
 print("Views: ", yt.views)
+
+yd = yt.streams.get_highest_resolution()  # Create downloader object
+
+yd.download('/Users/bobs/Desktop/Downloaded Vids/')  # run downloader
