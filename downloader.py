284ad27a269bde0e27c4232091c18b3557d3ad45
bob <c2064179@newcastle.ac.uk>
2023-10-31 11:24:25 +0000

    Create yt object from the link
    print statements to show the title and length of the video

--- downloader.py
+++ downloader.py
@@ -1,4 +1,8 @@
 from pytube import YouTube
 from sys import argv
 
-link = argv[1]
+link = argv[1]  # Link assigned to first argument from command line
+yt = YouTube(link)  # Create youtube object from the link
+
+print("Title: ", yt.title)
+print("Length: ", yt.length)
